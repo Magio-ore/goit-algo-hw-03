533a60f4b9e5bdcb1701b72fb6090de64b8aac0d
Magio-ore <74238314+Magio-ore@users.noreply.github.com>
2024-08-05 18:43:49 +0300

    Update Work1.py

--- Work1.py
+++ Work1.py
@@ -3,9 +3,15 @@ import datetime
 date = datetime.date(year=2023, month=7, day=3)
 
 def get_days_from_today(date):
-    date_now = datetime.date.today()
-    difference = date_now.toordinal() - date.toordinal()
-    return difference
+    try:
+        date_now = datetime.date.today()
+        difference = date_now.toordinal() - date.toordinal()
+        return difference
+    except AttributeError:
+        return "Неправильний формат дати. Будь ласка, введіть об'єкт типу datetime.date."
 
 
 print (get_days_from_today(date))
+
+invalid_date = "2023-07-03"
+print(get_days_from_today(invalid_date))
